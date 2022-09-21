653624c8c1b9704282505d8c6b57b996bcedb4e0
William Hiver <=>
2022-09-21 17:11:08 +0200

    Set coordinates using map

--- app/src/main/java/mobi/maptrek/fragments/CreateRoute.java
+++ app/src/main/java/mobi/maptrek/fragments/CreateRoute.java
@@ -16,6 +16,10 @@
 
 package mobi.maptrek.fragments;
 
+import static java.lang.Double.NaN;
+import static java.lang.Double.isNaN;
+import static java.lang.Double.parseDouble;
+
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,7 +31,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.graphhopper.ResponsePath;
+import com.graphhopper.util.shapes.GHPoint3D;
+
+import org.oscim.core.MapPosition;
+import org.oscim.event.Event;
+import org.oscim.map.Map;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -35,12 +45,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import mobi.maptrek.MainActivity;
+import mobi.maptrek.MapHolder;
 import mobi.maptrek.R;
+import mobi.maptrek.data.Track;
+import mobi.maptrek.data.source.FileDataSource;
 import mobi.maptrek.maps.routing.Router;
+import mobi.maptrek.util.StringFormatter;
 
-public class CreateRoute extends Fragment {
+public class CreateRoute extends Fragment implements Map.UpdateListener {
     private static String OSM_FILE_NAME = "bretagne.pbf";
     private Router mRouter;
+    private MapHolder mMapHolder;
+    TextInputEditText mFromField;
+    TextInputEditText mToField;
 
     public CreateRoute() {
         super(R.layout.fragment_create_route);
@@ -50,23 +68,91 @@ public class CreateRoute extends Fragment {
     @MainThread
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         view.findViewById(R.id.compute_route).setOnClickListener(v -> {
+            double fromLat = NaN;
+            double fromLon = NaN;
+            double toLat = NaN;
+            double toLon = NaN;
+
+            if (mFromField.getText().length() > 0 && mToField.getText().length() > 0) {
+                String[] parsedFrom = mFromField.getText().toString().split(" ");
+                String[] parsedTo = mToField.getText().toString().split(" ");
+                fromLat = parseDouble(parsedFrom[0]);
+                fromLon = parseDouble(parsedFrom[1]);
+                toLat = parseDouble(parsedTo[0]);
+                toLon = parseDouble(parsedTo[1]);
+            }
+
+            if (isNaN(fromLat) || isNaN(fromLon) || isNaN(toLat) || isNaN(toLon)) {
+                Toast.makeText(getContext(), "Fill From and To fields first.", Toast.LENGTH_LONG).show();
+                return;
+            }
+
             try {
-                double fromLat = 47.74064;
-                double fromLon = -3.38867;
-                double toLat = 47.74529;
-                double toLon = -3.35381;
                 ResponsePath route = mRouter.compute(getFileFromAssets(getContext(), OSM_FILE_NAME), fromLat, fromLon, toLat, toLon);
                 Log.d("Router", "Route distance: " + route.getDistance() + ", time: " + route.getTime());
-            } catch (IOException e) {
-                Toast.makeText(getContext(), "Could not load map file.", Toast.LENGTH_LONG);
+                ((MainActivity) getActivity()).showRoute(routeResponseToFileDataSource(route));
+            } catch (Exception e) {
+                Toast.makeText(getContext(), "Could not load route: " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
                 e.printStackTrace();
             }
         });
+
+        mFromField = view.findViewById(R.id.route_from);
+        mToField = view.findViewById(R.id.route_to);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        try {
+            mMapHolder = (MapHolder) context;
+            mMapHolder.getMap().events.bind(this);
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context + " must implement MapHolder");
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mMapHolder.getMap().events.unbind(this);
+    }
+
+    private void updateLocation(double latitude, double longitude) {
+        if (mFromField.isFocused()) {
+            mFromField.setText(StringFormatter.coordinates(0, " ", latitude, longitude));
+        } else if (mToField.isFocused()) {
+            mToField.setText(StringFormatter.coordinates(0, " ", latitude, longitude));
+        }
     }
 
     private String getFileFromAssets(Context context, String fileName) throws IOException {
         Path outFileName = Paths.get(context.getCacheDir().getAbsolutePath(), fileName);
         Files.copy(getResources().getAssets().open(OSM_FILE_NAME), outFileName, StandardCopyOption.REPLACE_EXISTING);
         return outFileName.toAbsolutePath().toString();
+    }
+
+    private FileDataSource routeResponseToFileDataSource(ResponsePath route) {
+        FileDataSource dataSource = new FileDataSource();
+        dataSource.name = getString(R.string.defaultCustomRouteName);
+        Track track = new Track();
+        dataSource.name = track.name;
+        dataSource.tracks.add(track);
+        track.source = dataSource;
+        dataSource.propertiesOffset = 0L;
+
+        for (GHPoint3D point : route.getPoints()) {
+            track.addPoint(/* continuous */ true, (int) (point.lat * 1E6), (int) (point.lon * 1E6), (float) point.ele, Float.NaN, Float.NaN, Float.NaN, 0L);
+        }
+
+        return dataSource;
+    }
+
+    @Override
+    public void onMapEvent(Event e, MapPosition mapPosition) {
+        if (e == Map.POSITION_EVENT) {
+            updateLocation(mapPosition.getLatitude(), mapPosition.getLongitude());
+        }
     }
 }
